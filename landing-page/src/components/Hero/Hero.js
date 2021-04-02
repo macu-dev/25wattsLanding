2024374292a752eaf81d5ab776bd358a50bb8b96
Maria Claudia <mariacladiaperezes@gmail.com>
2021-04-02 11:43:36 -0300

    se agrego los cambios

--- landing-page/src/components/Hero/Hero.js
+++ landing-page/src/components/Hero/Hero.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Carousel, Button } from "react-bootstrap";
-<<<<<<< HEAD:modulo19/landing-page/src/components/Hero/Hero.js
-import imageSlide from "../../assets/img/slider.png";
-
-=======
 import imageSlide from "../../assets/img/slider-1@2x.png";
->>>>>>> master:landing-page/src/components/Hero/Hero.js
 
 const Hero = () => {
   return (
@@ -20,11 +15,7 @@ const Hero = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-<<<<<<< HEAD:modulo19/landing-page/src/components/Hero/Hero.js
-          <img src={imageSlide} className='d-block w-100' alt='Third slide' />
-=======
           <img src={imageSlide} className='d-block w-100' alt='Second slide' />
->>>>>>> master:landing-page/src/components/Hero/Hero.js
           <Carousel.Caption>
             <h3>Sed ut perspiciatis <br></br> unde omnis iste natus</h3>
             <p>Sed ut perspiciatis unde omnis iste natus error <br></br> sit voluptatem accusantium doloremque.</p>
