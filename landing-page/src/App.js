5005486f842eedc9d9e74c1f4f08eeec7cc2f65e
Maria Claudia <mariacladiaperezes@gmail.com>
2021-04-02 11:30:54 -0300

    se elimino la carpeta modulo19

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.scss';
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import About from "./components/About/About"
+import Products from "./components/Products/Products"
+import Services from "./components/Services/Services"
+import Contact from "./containers/Contact/Contact"
+import Footer from "./components/Footer/Footer"
+// nueva prueba
+const App = ()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header/>
+       <Hero/>
+       <About/>
+       <Products/>
+       <Services/>
+       <Contact/>
+       <Footer/>
     </div>
   );
 }
