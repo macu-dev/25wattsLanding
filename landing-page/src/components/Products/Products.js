e627bd15de01e3390eb0c5a732a3865c578a2f64
Maria Claudia <mariacladiaperezes@gmail.com>
2021-04-05 01:22:44 -0300

    se agrego la coleccion products

--- landing-page/src/components/Products/Products.js
+++ landing-page/src/components/Products/Products.js
@@ -7,52 +7,41 @@ const Products = () =>{
     const products = [
         {
           title: 'Sed ut Sed ut perspiciatis 1',
-          subtile: 'Nemo Enim',
+          subtitle: 'Nemo Enim',
           text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam'  
+        },
+        {
+            title: 'Sed ut Sed ut perspiciatis 2',
+            subtitle: 'Nemo Enim',
+            text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam'  
+        },
+        {
+            title: 'Sed ut Sed ut perspiciatis 3',
+            subtitle: 'Nemo Enim',
+            text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam'  
         }
-
-
-
     ];
-
 
     return (
         <section id="products" className="section section-products">
            
            <div className="products-content">
             <Carousel className="products-content-cards" indicators={false}>
-                    <Carousel.Item className="pl-5 pr-5">
-                        <Carousel.Caption className="d-contents">
-                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 1</h3>
-                            <h2 className="carosuel-caption__subtitle m-0 pb-0">Nemo Enim</h2>
-                            <p className="carosuel-caption__text" >Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="pl-5 pr-5">
-                       
-                        <Carousel.Caption className="d-contents">
-                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 2</h3>
-                            <h2 className="carosuel-caption__subtitle m-0 pt-0">Nemo Enim</h2>
-                            <p className="carosuel-caption__text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="pl-5 pr-5">
-                        
-                        <Carousel.Caption className="d-contents">
-                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 3</h3>
-                            <h2 className="carosuel-caption__subtitle m-0 pt-0">Nemo Enim</h2>
-                            <p className="carosuel-caption__text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>      
+                {
+                    products.map(({title ,subtitle ,text}) => 
+                        <Carousel.Item className="pl-5 pr-5" key={title}>
+                            <Carousel.Caption className="d-contents">
+                                <h3 className="carosuel-caption__title m-0 pb-0">{title}</h3>
+                                <h2 className="carosuel-caption__subtitle m-0 pb-0">{subtitle}</h2>
+                                <p className="carosuel-caption__text" >{text}</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>  
+                    )
+                }
             </Carousel>
            </div>
-
-
-
         </section>
     )
 }
-
-
 
 export default Products;
