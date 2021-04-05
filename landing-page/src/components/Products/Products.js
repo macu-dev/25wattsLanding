8e856601e49e3148a80a35b9b9b72614cb8d7118
Maria Claudia <mariacladiaperezes@gmail.com>
2021-04-05 00:50:33 -0300

    se agrego las clases carosuel caption title, text y subtitle

--- landing-page/src/components/Products/Products.js
+++ landing-page/src/components/Products/Products.js
@@ -3,34 +3,34 @@ import Carousel from 'react-bootstrap/Carousel';
 
 
 const Products = () =>{
+
+
     return (
         <section id="products" className="section section-products">
            
            <div className="products-content">
-
-
-            <Carousel className="products-content-cards">
-                    <Carousel.Item>
-                        <Carousel.Caption>
-                            <h3>Sed ut perspiciatis 1</h3>
-                            <h2>Nemo Enim</h2>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            <Carousel className="products-content-cards" indicators={false}>
+                    <Carousel.Item className="pl-5 pr-5">
+                        <Carousel.Caption className="d-contents">
+                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 1</h3>
+                            <h2 className="carosuel-caption__subtitle m-0 pb-0">Nemo Enim</h2>
+                            <p className="carosuel-caption__text" >Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
+                    <Carousel.Item className="pl-5 pr-5">
                        
-                        <Carousel.Caption>
-                            <h3>Sed ut perspiciatis 2</h3>
-                            <h2>Nemo Enim</h2>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        <Carousel.Caption className="d-contents">
+                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 2</h3>
+                            <h2 className="carosuel-caption__subtitle m-0 pt-0">Nemo Enim</h2>
+                            <p className="carosuel-caption__text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
-                    <Carousel.Item>
+                    <Carousel.Item className="pl-5 pr-5">
                         
-                        <Carousel.Caption>
-                            <h3>Sed ut perspiciatis 3</h3>
-                            <h2>Nemo Enim</h2>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                        <Carousel.Caption className="d-contents">
+                            <h3 className="carosuel-caption__title m-0 pb-0">Sed ut perspiciatis 3</h3>
+                            <h2 className="carosuel-caption__subtitle m-0 pt-0">Nemo Enim</h2>
+                            <p className="carosuel-caption__text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
                         </Carousel.Caption>
                     </Carousel.Item>      
             </Carousel>
