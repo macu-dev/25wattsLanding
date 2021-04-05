a71284798c93ef2618df718a45bf6eee2f357558
Maria Claudia <mariacladiaperezes@gmail.com>
2021-04-05 13:24:39 -0300

    indentacion del codigo

--- landing-page/src/components/About/About.js
+++ landing-page/src/components/About/About.js
@@ -4,15 +4,15 @@ import React from 'react';
 const About = () =>{
     return (
         <section id="about">
-                <div className="about-container">
-                            <div className="about-container--H2">
-                                <h2>We Help Businesses <span>Grow and Innovate</span></h2>
-                            </div>
-                            <div className="about-container--P">
-                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut <br></br> labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et <br></br> ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem <br></br> ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et <br></br> dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea <br></br> rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <br></br> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
-                            </div>
-                    
+            <div className="about-container">
+                <div className="about-container--H2">
+                    <h2>We Help Businesses <span>Grow and Innovate</span></h2>
                 </div>
+                <div className="about-container--P">
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut <br></br> labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et <br></br> ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem <br></br> ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et <br></br> dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea <br></br> rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <br></br> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                </div>
+                
+            </div>
         </section>
     )
 }
